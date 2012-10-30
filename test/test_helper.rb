e9dfd4ae5d02762ad7068a8a13a56fff460a319a
Matt Aimonetti <mattaimonetti@gmail.com>
2012-10-29 18:11:16 -0700

    ok now the tests should run (and fail) on 1.8

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,9 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup
 require 'test/unit'
+if RUBY_VERSION.to_f < 1.9
+  require 'minitest/autorun'
+end
 
 $: << File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'wd_sinatra'
