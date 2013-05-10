e8bb69a4a2ae051ea342feefa5f404920a0745b4
Jack Chu <kamuigt@gmail.com>
2013-05-10 01:04:57 -0400

    if Accept header is nil, select the first defined service format as wd.format and set wd.media_type to */*

--- templates/lib/hooks.rb
+++ templates/lib/hooks.rb
@@ -52,12 +52,18 @@ module WDSinatraHooks
   # Implementation example
   def pre_dispatch_hook
     # content negotiation
-    require "rack/accept"
     accept = env["rack-accept.request"]
-    service_media_types = SUPPORTED_MEDIA_TYPES.select {|k,v| service.formats.include?(v)}
-    env["wd.media_type"] = accept.media_type.best_of(service_media_types.keys)
-    halt 406 unless env["wd.media_type"]
-    env["wd.format"] =  SUPPORTED_MEDIA_TYPES[env["wd.media_type"]]
+    if accept
+      service_media_types = SUPPORTED_MEDIA_TYPES.select {|k,v| service.formats.include?(v)}
+      env["wd.media_type"] = accept.media_type.best_of(service_media_types.keys)
+      halt 406 unless env["wd.media_type"]
+      env["wd.format"] =  SUPPORTED_MEDIA_TYPES[env["wd.media_type"]]
+    else
+      env["wd.media_type"] = "*/*"
+      env["wd.format"] =  service.formats.first
+      halt 406 unless env["wd.format"] # 406 if no defined service.formats
+    end
+
     content_type env["wd.format"], charset: "utf-8"
 
     if service.extra[:mobile]
